20a1358d3fecd15442dabd96d8f0d387471751c6
Jason Goodsell <jagoodse@microsoft.com>
2019-03-01 22:41:53 +1100

    Updated insert_record function in table_client.py.

--- src/autotrainer/autotrainer/table/table_client.py
+++ src/autotrainer/autotrainer/table/table_client.py
@@ -1,4 +1,5 @@
 from azure.cosmosdb.table.tableservice import TableService
+from azure.cognitiveservices.vision.customvision.training.models import ImageCreateResult
 from urllib.parse import urlparse
 
 
@@ -19,11 +20,11 @@ class TableClient:
         if self.table_service.exists(self.table_name) is False:
             self.table_service.create_table(self.table_name)
 
-    def insert_record(self, item):
-        url = urlparse(item.download_url)
+    def insert_record(self, item: ImageCreateResult, container_name: str):
+        url = urlparse(item.source_url)
         keys = url.path.split("/")
-        record = {'PartitionKey': keys[1], 'RowKey': keys[2]}
-        self.table_service.insert_entity(self.table_name, record)
+        record = {'PartitionKey': container_name, 'RowKey': item.image.id, 'url': keys[0] + keys[1]}
+        self.table_service.insert_or_replace_entity(self.table_name, record)
 
 
 def create_table_client(account_name: str, key: str) -> TableClient:
