499f08e00071b0ea483d9a4d64e91ad903354495
Jason Goodsell <jagoodse@microsoft.com>
2019-03-01 16:47:20 +1100

    Insert record of image in table from autotrainer after uploading image.

--- src/autotrainer/autotrainer/autotrainer.py
+++ src/autotrainer/autotrainer/autotrainer.py
@@ -1,5 +1,6 @@
 from azure.cognitiveservices.vision.customvision.training.models import ImageCreateResult
 from autotrainer.custom_vision.custom_vision_client import CustomVisionClient, create_cv_client
+from autotrainer.table.table_client import TableClient, create_table_client_from_connection_string
 from autotrainer.blob.blob_client import BlobClient, create_blob_client_from_connection_string
 from autotrainer.blob.models.container import Container
 from autotrainer.blob.models.labelled_blob import LabelledBlob
@@ -12,6 +13,7 @@ class Autotrainer:
     def __init__(self, cv_key: str, cv_endpoint: str, storage_connection_string:str):
         self.custom_vision = create_cv_client(cv_endpoint, cv_key)
         self.blob = create_blob_client_from_connection_string(storage_connection_string)
+        self.table = create_table_client_from_connection_string(storage_connection_string)
 
     def get_file_paths(self, directory_path: str, ext: str = '')->[str]:
         return list_paths(directory_path, ext)
@@ -23,6 +25,10 @@ class Autotrainer:
         labelled_blobs = []
         for path in image_paths:
             labelled_blobs.append(self.blob.add_data_from_path(container.value, path, labels, parent ))
+
+        for meta in labelled_blobs:
+            self.table.insert_record(meta)
+
         return labelled_blobs
 
     def add_all_images_to_cv(self, container: Container, projectId: str, num_results: int = None)->[ImageCreateResult]:
