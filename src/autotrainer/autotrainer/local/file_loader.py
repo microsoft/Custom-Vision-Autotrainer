bec465da82e312ecfd70fa27bbfa04fcf62fd7bc
Jason Goodsell <jagoodse@microsoft.com>
2019-03-02 23:24:17 +1100

    Ignore all files starting with `.` from uploading to blob storage.

--- src/autotrainer/autotrainer/local/file_loader.py
+++ src/autotrainer/autotrainer/local/file_loader.py
@@ -1,9 +1,7 @@
-
 import os
+
 
 def list_paths(directory_path: str, ext: str = ''):
     for file in os.listdir(directory_path):
-        if os.path.isfile(os.path.join(directory_path, file)) and file.endswith(ext):
+        if os.path.isfile(os.path.join(directory_path, file)) and file.endswith(ext) and not file.startswith('.'):
             yield os.path.join(directory_path, file)
-
-    
