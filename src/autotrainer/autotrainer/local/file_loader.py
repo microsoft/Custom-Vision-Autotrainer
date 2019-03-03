92ad5af820988778158aa1bfb7c739ff8e9ce9d9
Jason Goodsell <jagoodse@microsoft.com>
2019-03-03 14:12:28 +1100

    Added function for returning a list of annotation files for given set.

--- src/autotrainer/autotrainer/local/file_loader.py
+++ src/autotrainer/autotrainer/local/file_loader.py
@@ -6,4 +6,11 @@ def list_paths(directory_path: str, ext: str = ''):
         if os.path.isfile(os.path.join(directory_path, file)) and file.endswith(ext):
             yield os.path.join(directory_path, file)
 
-    
+
+def list_yolo_annotation_paths(annotation_path: str, image_paths: [str]):
+    annotations = []
+    for path in image_paths:
+        filename = os.path.splitext(os.path.basename(path))[0]
+        loc = os.path.join(annotation_path, filename + ".txt")
+        annotations.append(loc)
+    return annotations
